63bb091c7f834465c5228bc08380035aef0fada5
federico_viganò <fede_viga@hotmail.it>
2024-04-23 19:16:32 +0200

    aggiunto delete - fine bonus 2 - fine esercizio

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -12,13 +12,44 @@
 
       @foreach($types as $type)
       <li class="d-flex justify-content-between align-items-center list-group-item list-group-item" aria-current="true">
-      <span>{{$type->title}}</span>
-      <div>
-        <a href="{{route('types.edit', $type->id)}}" type="button" class="btn btn-sm btn-outline-warning">Edit</a>
-        <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
-      </div>
+
+        <span>{{$type->title}}</span>
+
+        <div>
+          <a href="{{route('types.edit', $type->id)}}" type="button" class="btn btn-sm btn-outline-warning">Edit</a>
+
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-sm btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteTypeModal">
+            Delete
+          </button>
+
+          <!-- Modal -->
+          <div class="modal" tabindex="-1" id="deleteTypeModal">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Delete Project</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <p>Are you sure you want to delete {{$type->title}}?</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <form action="{{route('types.destroy', $type->id)}}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger">Delete</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </li>
       @endforeach
+
+
       <div class="pt-3">
         <a href="{{route('types.create')}}" class="btn btn-outline-light" href="">Add Type</a>
       </div>
